e1e3a619c3bbf554f46a4874aaaab302fe1ca42c
Anto Martínez <antomartinezdev@gmail.com>
2024-03-25 15:27:15 +0100

    He hecho correcciones gramaticales en los comentarios para JAVADOC

--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -2,7 +2,7 @@ package main;
 import java.util.Scanner;
 
 /**
- * Clase Calculadora para hacer calculos sencillos con numeros.
+ * La clase Calculadora para hacer calculos sencillos con numeros.
  * Las operaciones que se pueden realizar en esta versión son: Suma, Resta, Multiplicación y División.
  * 
  * @version 1.0 25 Marzo 2024
@@ -15,19 +15,19 @@ public class Calculadora {
 
         System.out.println("Introduce el primer número:");
         /**
-         * 
+         * Primer numero para operar en la calculadora
          */
         double num1 = scanner.nextDouble();
 
         System.out.println("Introduce el segundo número:");
         /**
-         * 
+         * Segundo numero para operar en la calculadora
          */
         double num2 = scanner.nextDouble();
 
         System.out.println("Elige la operación (suma, resta, multiplicación, división):");
         /**
-         * 
+         * Tipo de operacion elegida a realizar
          */
         String operacion = scanner.next();
 
@@ -55,40 +55,40 @@ public class Calculadora {
     }
 	
 	/**
-	 * Devuelve la suma de dos numeros double.
+	 * Realiza la suma de dos numeros double.
 	 * @param a es el primer numero a sumar.
 	 * @param b es el segundo numero a sumar.
-	 * @return El resultado de la suma.
+	 * @return El resultado de la suma a+b.
 	 */
 	public static double suma(double a, double b) {
         return a + b;
     }
 	
 	/**
-	 * Devuelve la resta de dos numeros double.
+	 * Realiza la resta de dos numeros double.
 	 * @param a es el primer numero a restar.
 	 * @param b es el segundo numero a restar.
-	 * @return El resultado de la resta.
+	 * @return El resultado de la resta a-b.
 	 */
     public static double resta(double a, double b) {
         return a - b;
     }
     
     /**
-	 * Devuelve la multiplicacion de dos numeros double.
+	 * Realiza la multiplicacion de dos numeros double.
 	 * @param a es el primer numero a multiplicar.
 	 * @param b es el segundo numero a multiplicar.
-	 * @return El resultado de la multiplicacion.
+	 * @return El resultado de la multiplicacion a*b.
 	 */
     public static double multiplicacion(double a, double b) {
         return a * b;
     }
     
     /**
-	 * Devuelve la division de dos numeros double.
+	 * Realiza la division de dos numeros double.
 	 * @param a es el dividendo.
 	 * @param b es el divisor - b > 0.
-	 * @return el resultado de la suma
+	 * @return el resultado de la division a/b.
 	 */
     public static double division(double a, double b) {
         return a / b;
